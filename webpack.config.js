237d92435bfa38edf6d1693a4c5b14eb8ddbf734
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-08-30 17:25:11 -0700

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const HtmlWebpackPlugin  = require('html-webpack-plugin');
-
 
 module.exports = {
-    entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output management',
-        }),
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    } ,
+  },
+
 };
